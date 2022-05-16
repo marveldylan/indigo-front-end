df92ed37008a1be1894d5a53132e16dfc09e03d4
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-16 01:23:48 -0400

    added apiURL for prodution

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const BASE_URL = 'http://localhost:3001';
 
+let apiUrl = process.env.NODE_ENV === 'production' ? 'https://indigo-back-end.herokuapp.com/' : 'http://localhost:3001'
+
 const Client = axios.create({ baseURL: BASE_URL });
 
 Client.interceptors.request.use(
