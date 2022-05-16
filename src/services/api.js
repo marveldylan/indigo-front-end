9023ceacc99334135599073d2c456b2b6d085f4e
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-16 02:38:19 -0400

    added console log to check path

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export const BASE_URL = 'http://localhost:3001';
 let apiUrl = process.env.NODE_ENV === 'production' ? 'https://indigo-back-end.herokuapp.com/' : 'http://localhost:3001'
 
 const Client = axios.create({ baseURL: apiUrl });
-
+console.log(apiUrl)
 Client.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('token')
