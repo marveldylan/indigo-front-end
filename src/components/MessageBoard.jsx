c12ce5432b515995b1d99043643b8f3e4ca86b96
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-05-16 10:58:22 -0400

    fixed spelling errors

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -4,8 +4,8 @@ const MessageBoard = () => {
     return (
         <div className="Message-board">
             <img className="Message-image" src="https://i.imgur.com/mccZ2Qs.png" />
-            <h3>Welcome</h3>
-            <p>project_indigo is a new kind of social media platform that connects builders, creatives, hobbysts, explorers, enthusiasts, professionals and amateurs to share knowledge and build communities around collective skills and passions. We're interested in taking infotainment to a whole new level, with professional and user-created channels, content, and tutorials on everything from audio engineering to how to string a tennis racket.</p>
+            <h3>Here. We. Go.</h3>
+            <p>project_indigo is a new kind of social media platform that connects builders, creatives, hobbyists, explorers, enthusiasts, professionals and amateurs to share knowledge and build communities around collective skills and passions. We're interested in taking infotainment to a whole new level, with professional and user-created channels, content, and tutorials on everything from audio engineering to how to string a tennis racket.</p>
             <h4>Who Are We?</h4>
             <p>At our core, project_indigo is a rag-tag bunch of goons that like to learn. We aim to be a positive force, and create a social media experience that focuses on interesting content facilitating personal growth, rather than . . . well, mindless junk. We hope for users to build meaningful connections with others who share their passions, and explore new things too!</p>
         </div>
